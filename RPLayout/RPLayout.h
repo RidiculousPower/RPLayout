747a28ea81884754fc969e7a3102307b7cde2e2d
Asher Haig <asher@ridiculouspower.com>
2014-02-19 05:36:12 -0500

    Initial Commit

---
+++ RPLayout/RPLayout.h
@@ -0,0 +1,13 @@
+//
+//  RPLayout.h
+//  RPLayout
+//
+//  Created by Asher on 2/19/14.
+//  Copyright (c) 2014 Ridiculous Power. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RPLayout : NSObject
+
+@end
---
+++ RPLayout/RPLayout.m
@@ -0,0 +1,13 @@
+//
+//  RPLayout.m
+//  RPLayout
+//
+//  Created by Asher on 2/19/14.
+//  Copyright (c) 2014 Ridiculous Power. All rights reserved.
+//
+
+#import "RPLayout.h"
+
+@implementation RPLayout
+
+@end
