fb16d2a66ff725bea8c30ad986aad2f2e5425c66
Asher Haig <asher@ridiculouspower.com>
2014-02-21 18:51:31 -0500

    added prototypes

--- include/RPLayout/RPLayoutController.h
+++ include/RPLayout/RPLayoutController.h
@@ -47,6 +47,23 @@
 	- (RPLayoutConstraint*) constraintWithIdentifier: (NSString*)  identifier
                                          forObject: (id)         constrained_object;
 
+	- (RPLayoutConstraint*)    constraintOn: (id)         constrained_object
+	                         withIdentifier: (NSString*)  identifier;
+	- (RPLayoutConstraint*)    constraintOnReturn: (SEL)        constrained_object_method
+	                               withIdentifier: (NSString*)  identifier;
+	- (RPLayoutConstraint*)    constraintOn: (id)         constrained_object
+	                          constrainedBy: (id)         constraining_object
+	                         withIdentifier: (NSString*)  identifier;
+	- (RPLayoutConstraint*)    constraintOn: (id)         constrained_object
+	                    constrainedByReturn: (SEL)        constraining_object_method
+	                         withIdentifier: (NSString*)  identifier;
+	- (RPLayoutConstraint*)    constraintOnReturn: (SEL)        constrained_object_method
+	                                constrainedBy: (id)         constraining_object
+	                               withIdentifier: (NSString*)  identifier;
+	- (RPLayoutConstraint*)    constraintOnReturn: (SEL)        constrained_object_method
+	                          constrainedByReturn: (SEL)        constraining_object_method
+	                               withIdentifier: (NSString*)  identifier;
+
   - (void) updateConstraintWithIdentifier: (NSString*) identifier;
   - (void) removeConstraintWithIdentifier: (NSString*) identifier;
   - (void) updateConstraints;
